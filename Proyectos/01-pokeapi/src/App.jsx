fa2d562b81c926adbf0e2dffa8e2197d2d327782
Salvarobles <sarogo36@gmail.com>
2024-01-19 21:26:52 +0100

    routes pokeApi

--- Proyectos/01-pokeapi/src/App.jsx
+++ Proyectos/01-pokeapi/src/App.jsx
@@ -1,11 +1,19 @@
-
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import NavBar from "./components/Navbar/NavBar";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
-
+      <NavBar />
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      
     </>
   );
 }
--- Proyectos/01-pokeapi/src/components/Navbar/NavBar.jsx
+++ Proyectos/01-pokeapi/src/components/Navbar/NavBar.jsx
@@ -1,10 +1,11 @@
-
-
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <>
+      <Link to="/about"> About</Link>
 
+      <Link to="/">Home</Link>
     </>
   );
 };
